0668a1ade0c2340bb4853d01dd5d696058b8705c
Erickjllo <erickjllo07@gmail.com>
2025-10-28 22:58:14 -0500

    ubicar personas aleatoriamente en el tablero

--- src/model/simulacion.py
+++ src/model/simulacion.py
@@ -17,3 +17,20 @@ class Simulacion:
         self.arbol: ArbolInfectado = ArbolInfectado()
 
         print(f"Simulación creada con tablero {tamano}x{tamano} y {num_personas} personas.")
+        
+        posiciones_ocupadas: set[tuple[int, int]] = set()
+        for i in range(1, num_personas + 1):
+            while True:
+                x = random.randint(0, tamano - 1)
+                y = random.randint(0, tamano - 1)
+                if (x, y) not in posiciones_ocupadas:
+                    posiciones_ocupadas.add((x, y))
+                    break
+            persona = Persona(i, x, y)
+            self.personas.append(persona)
+            celda = self.tablero.matriz[x][y]
+            celda.ocupada = True
+            celda.ocupante = persona
+            celda.estado = "sano"
+
+        print(f"{len(self.personas)} personas ubicadas aleatoriamente en el tablero.")
