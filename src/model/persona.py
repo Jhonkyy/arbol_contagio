beec1ad004546ff3cfdd6f0bc19f1654db197efe
Erickjllo <erickjllo07@gmail.com>
2025-10-28 21:11:57 -0500

    modificar clase Persona para incluir coordenadas x e y en el constructor

--- src/model/persona.py
+++ src/model/persona.py
@@ -1,5 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
 class Persona:
-    def __init__(self, id:int):
+    
+    def __init__(self, id: int, x: int = 0, y: int = 0) -> None:
         self.id: int = id
+        self.x: int = x
+        self.y: int = y
         self.nivel_defensa: int = 3
         self.estado: str = "sano"
