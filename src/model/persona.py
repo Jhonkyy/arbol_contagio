91153d910266b7e0136d5d2ac612e71e0d347d18
Jhonkyyy <jhonfre0212@gmail.com>
2025-10-31 03:44:32 -0500

    agregar repr a la clase personas , para colores

--- src/model/persona.py
+++ src/model/persona.py
@@ -9,3 +9,9 @@ class Persona:
         self.y: int = y
         self.nivel_defensa: int = 3
         self.estado: str = "sano"
+        
+    def __repr__(self):
+        if self.estado == "sano":
+            return f"\033[92mp{self.id}\033[0m"  
+        else:
+            return f"\033[91mp{self.id}\033[0m"  
